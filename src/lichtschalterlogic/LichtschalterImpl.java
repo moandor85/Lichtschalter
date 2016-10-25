b4853a4e646c02bc98b6547df17a1fc0ffd3a085
moandor <moandor85@gmail.com>
2016-10-25 21:23:57 +0200

    organized unused imports in LichtschalterImpl

--- src/lichtschalterlogic/LichtschalterImpl.java
+++ src/lichtschalterlogic/LichtschalterImpl.java
@@ -1,11 +1,8 @@
 package lichtschalterlogic;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
