f52e5566f82e589e9b4027dec77f1b19a6d6bf0c
moandor <moandor85@gmail.com>
2016-11-03 22:10:33 +0100

    Fixed Buggy Switch on in Command

--- src/de/moandor/lightswitch/userinterface/ConsoleImpl.java
+++ src/de/moandor/lightswitch/userinterface/ConsoleImpl.java
@@ -18,15 +18,17 @@ public class ConsoleImpl implements Console {
 		while (true) {
 			System.out.print(ConsoleCommands.ENTER_COMMAND);
 		    String inputLN = sc.nextLine();
-		    
+		    //Command for halt!
 		    if(inputLN.equals(ConsoleCommands.EXIT)){
 		    	break;
 		    }
+		    // Command for off
 		    else if(inputLN.equals(ConsoleCommands.SWITCH_LIGHT_OFF)){
 		    	ls.switchLightOff();
-		    	System.out.println(ConsoleCommands.LIGHT_SWITCHED_ON);
+		    	System.out.println(ConsoleCommands.LIGHT_SWITCHED_OFF);
 		    }
-		    else if(inputLN.equals(ConsoleCommands.SWITCH_LIGHT_OFF)){
+		    //Command for on 
+		    else if(inputLN.equals(ConsoleCommands.SWITCH_LIGHT_ON)){
 		    	ls.switchLightOn();
 		    	System.out.println(ConsoleCommands.LIGHT_SWITCHED_ON);
 		    }
